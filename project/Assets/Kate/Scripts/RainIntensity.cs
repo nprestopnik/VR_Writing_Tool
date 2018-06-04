e7c27f6ab3338bfeedff683274ffa1bb26678876
Kate Polley <kpolley@ithaca.edu>
2018-06-04 18:02:03 -0400

    beginning rain particle system

---
+++ project/Assets/Kate/Scripts/RainIntensity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainIntensity : MonoBehaviour {
+
+	[Range(0,300)]
+	public float rainIntensity = 0f;
+	public ParticleSystem rain;
+
+	void Update () {
+		var emissionMod = rain.emission;
+		emissionMod.rate = rainIntensity;
+	}
+}
