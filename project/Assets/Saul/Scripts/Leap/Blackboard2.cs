d33cb4cbef1ed2c4b2afd1ab9ca53bcc81143321
Saul Amster <saulamster@gmail.com>
2018-06-05 12:57:45 -0400

    Added: Resizing Whiteboard

--- project/Assets/Saul/Scripts/Leap/Blackboard2.cs
+++ project/Assets/Saul/Scripts/Leap/Blackboard2.cs
@@ -36,15 +36,17 @@ public class Blackboard2 : MonoBehaviour {
 		pointer.GetComponent<MeshRenderer>().material = (lMat);
 		SetGlobalScale(pointer.transform, Vector3.one * lineWidth);
 
-		// boardScaler.transform.localScale = (boardScaler.transform.position - scaleHandle.transform.position);
-		// boardScaler.transform.localScale = new Vector3(boardScaler.transform.localScale.x, -2* boardScaler.transform.localScale.y, -1 * boardScaler.transform.localScale.z);
-		//SetGlobalScale(boardScaler.transform, boardScaler.transform.position - scaleHandle.transform.position);
+		boardScaler.transform.localScale = (scaleHandle.transform.localPosition);// * transform.root.localScale.x;
+		boardScaler.transform.localScale = new Vector3(boardScaler.transform.localScale.x * -1 , boardScaler.transform.localScale.y, 1);
+		//SetGlobalScale(boardScaler.transform, scaleHandle.transform.localPosition);
 		scaleHandle.transform.localPosition = new Vector3(scaleHandle.transform.localPosition.x, scaleHandle.transform.localPosition.y, 0);
 		scaleHandle.transform.localRotation = Quaternion.identity;
 
 		if(currLineR != null) {
-			Vector3 newPos = transform.root.rotation * Vector3.Scale(pointer.transform.localPosition , transform.root.localScale) - (transform.forward * lineWidth / 2);
+			Vector3 newPos = pointer.transform.localPosition;//transform.root.rotation * Vector3.Scale(pointer.transform.localPosition, transform.root.localScale) - (transform.forward * lineWidth / 2);
 			currLineR.SetPositions(points.ToArray());
+			currLineR.transform.localRotation = Quaternion.Euler(0,0,0);
+			currLineR.transform.localScale = Vector3.one;
 			currLineR.positionCount = points.Count;
 			currLineR.SetPosition(currLineR.positionCount - 1, newPos);
 			points.Add(newPos);
@@ -60,16 +62,20 @@ public class Blackboard2 : MonoBehaviour {
 		currLineR.endWidth = lineWidth;
 		currLineR.material = lMat;
 		currLineR.useWorldSpace = false;
+		currLineR.alignment = LineAlignment.Local;
 		points = new List<Vector3>();
-		points.Add(transform.root.rotation * Vector3.Scale(pointer.transform.localPosition, transform.root.localScale));
+		points.Add(pointer.transform.localPosition);
+		
 
-		go.transform.SetParent(transform.root);
+		go.transform.SetParent(transform.parent);
 		go.transform.localPosition = Vector3.zero;
 	}
 
 	public void end(){
 		//print("end");
-		currLineR.transform.SetParent(transform.parent);
+		//currLineR.transform.SetParent(transform.parent);
+		//currLineR.transform.localScale = Vector3.zero;
+		currLineR.transform.localRotation = Quaternion.Euler(0,0,0);
 		//currLineR.transform.localPosition = transform.root.position - transform.position;
 		currLineR = null;
 	}
