80c3a2c4c52c42840b77cc4bcaa8fbf7a3efcd24
Saul Amster <saulamster@gmail.com>
2018-06-06 16:07:06 -0400

    Whiteboard Undo/Redo

--- project/Assets/Master/Scripts/Leap/Blackboard.cs
+++ project/Assets/Master/Scripts/Leap/Blackboard.cs
@@ -17,13 +17,18 @@ public class Blackboard : MonoBehaviour {
 	
 
 	private LineRenderer currLineR;
+	private LineData currData;
 	private List<Vector3> points;
+	private List<LineData> history;
+	private List<LineData> redoHistory;
 
 	private int numLines = 0;
 
 	// Use this for initialization
 	void Start () {
 		button = GetComponent<Leap.Unity.Interaction.InteractionButton>();
+		history = new List<LineData>();
+		redoHistory = new List<LineData>();
 	}
 	
 	// Update is called once per frame
@@ -66,6 +71,7 @@ public class Blackboard : MonoBehaviour {
 	public void begin(){
 		GameObject go = new GameObject (); 
 
+		currData = go.AddComponent<LineData>();
 		currLineR = go.AddComponent<LineRenderer>();
 		currLineR.startWidth = lineWidth;
 		currLineR.endWidth = lineWidth;
@@ -78,6 +84,8 @@ public class Blackboard : MonoBehaviour {
 		currLineR.sortingOrder = numLines;
 		numLines++;
 
+		redoHistory = new List<LineData>();
+
 		go.transform.SetParent(transform.parent);
 		go.transform.localPosition = Vector3.zero;
 	}
@@ -85,8 +93,12 @@ public class Blackboard : MonoBehaviour {
 	//Called when contact ends with the board
 	public void end(){
 		currLineR.transform.localRotation = Quaternion.Euler(0,0,0);
-
+		currData.lineWidth = lineWidth;
+		currData.lMat = lMat;
+		currData.points = points.ToArray();
+		history.Add(currData);
 		currLineR = null;
+		currData = null;
 	}
 
 	//Used to set the line width
@@ -107,5 +119,53 @@ public class Blackboard : MonoBehaviour {
 	{
 		transform.localScale = Vector3.one;
 		transform.localScale = new Vector3 (globalScale.x/transform.lossyScale.x, globalScale.y/transform.lossyScale.y, globalScale.z/transform.lossyScale.z);
+	}
+
+	public void Undo() {
+		LineData l = history[history.Count - 1];
+		redoHistory.Add(l);
+		Destroy(l.gameObject);
+		history.RemoveAt(history.Count - 1);
+	}
+
+	public void Redo() {
+		if(redoHistory.Count > 0) {
+			LineData l = redoHistory[redoHistory.Count - 1];
+			
+			GameObject go = new GameObject (); 
+
+			currData = go.AddComponent<LineData>();
+			currData.lineWidth = l.lineWidth;
+			currData.lMat = l.lMat;
+			currData.points = l.points;
+
+			currLineR = go.AddComponent<LineRenderer>();
+			currLineR.startWidth = l.lineWidth;
+			currLineR.endWidth = l.lineWidth;
+			currLineR.material = l.lMat;
+			currLineR.useWorldSpace = false;
+			currLineR.alignment = LineAlignment.Local;
+			//points = new List<Vector3>();
+			//points.Add(pointer.transform.localPosition);
+			
+			currLineR.sortingOrder = numLines;
+			numLines++;
+
+			currLineR.positionCount = l.points.Length - 1;
+			currLineR.SetPositions(l.points);
+			
+
+			go.transform.SetParent(transform.parent);
+			go.transform.localPosition = Vector3.zero;
+			currLineR.transform.localRotation = Quaternion.Euler(0,0,0);
+			currLineR.transform.localScale = Vector3.one;
+
+			//end();
+			redoHistory.RemoveAt(redoHistory.Count - 1);
+			history.Add(currData);
+			currLineR = null;
+			currData = null;
+		}	
+		
 	}
 }
