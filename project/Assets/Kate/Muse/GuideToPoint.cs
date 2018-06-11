9087009ca10d8520a38ba7e736d2defd4a59b7b0
Kate Polley <kpolley@ithaca.edu>
2018-06-11 17:48:33 -0400

    starting muse in test environment

---
+++ project/Assets/Kate/Muse/GuideToPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideToPoint : MonoBehaviour {
+
+	public Transform target;
+	public float guideSpeed = 5.0f;
+	public float activationSpeed = 5.0f;
+	public bool guiding = false;
+	public bool activation = false;
+
+	private Vector3 pos;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (guiding) {
+			if(transform.position != target.position) {
+				if (activation) {
+					pos = Vector3.MoveTowards(transform.position, target.position, activationSpeed * Time.deltaTime);
+				}
+				else {
+					pos = Vector3.MoveTowards(transform.position, target.position, guideSpeed * Time.deltaTime);
+				}
+				GetComponent<Rigidbody>().MovePosition(pos);
+			} else {
+				guiding = false;
+				activation = false;
+			}
+		}
+	}
+}
---
+++ project/Assets/Kate/Muse/MuseController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuseController : MonoBehaviour {
+
+	public Transform idlePoint;
+	public Transform spawnPoint;
+	public Transform targetPoint;
+
+	public float floatStrength = 1;
+	
+	private float originalY;
+	private GuideToPoint guide;
+
+	void Start () {
+		guide = GetComponent<GuideToPoint>();
+		originalY = transform.position.y;
+	}
+	
+	void Update () {
+
+		//check to see if the muse is moving to a point - only hover when not moving
+		bool guiding = GetComponent<GuideToPoint>().guiding;
+		if (!guiding){
+			transform.position = new Vector3(transform.position.x, 
+				originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
+				transform.position.z);
+		} else {
+			originalY = transform.position.y;
+		}
+
+	}
+
+	public void ActivateMuse() {
+		guide.activation = true;
+		guide.guiding = true;
+		guide.target = idlePoint;
+	}
+
+	public void DeactivateMuse(){
+		guide.activation = true;
+		guide.guiding = true;
+		guide.target = spawnPoint;
+	}
+
+	public void MuseGuide(){
+		guide.guiding = true;
+		guide.target = targetPoint;
+	}
+}
+
