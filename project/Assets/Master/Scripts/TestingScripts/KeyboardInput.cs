da821c370744e6b06d301b287d4062c58c497ec4
Kate Polley <kpolley@ithaca.edu>
2018-06-22 17:39:22 -0400

    testing typing and preparing for keyboard positioning

---
+++ project/Assets/Master/Scripts/TestingScripts/KeyboardInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardInput : MonoBehaviour {
+
+	public InputField testInput;
+	
+	void Start () {
+		testInput.ActivateInputField();
+	}
+	
+}
