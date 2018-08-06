28867a386dda7188bfcac987501ec8eb728d3a9f
Kate Polley <kpolley@ithaca.edu>
2018-08-06 17:25:04 -0400

    started looking at hand covering

---
+++ project/Assets/Master/Scripts/Menu/HandOcclusion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandOcclusion : MonoBehaviour {
+
+	public bool handCovering = false;
+
+	void OnTriggerEnter(Collider col) {
+		//collide with hands but not camera capsule collider
+		if(col.tag == "Player" && col.name != "Capsule") {
+			handCovering = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if(col.tag == "Player" && col.name != "Capsule") {
+			handCovering = false;
+		}
+	}
+}
